dbd7d7c1afad1e2011bd876e925dbc4768c1f96f
Lars Leijten <larsleijten@gmail.com>
2024-12-02 16:48:40 +0100

    Day 2 part 2

--- src/main/java/com/adventofcode/solutions/Day02.java
+++ src/main/java/com/adventofcode/solutions/Day02.java
@@ -3,15 +3,32 @@ package com.adventofcode.solutions;
 import com.adventofcode.utils.InputReader;
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day02 {
   public Day02() throws URISyntaxException, FileNotFoundException {
     List<List<Long>> reports = InputReader.readAsLongMatrix("/day02.txt");
+    System.out.println(reports.stream().filter(this::checkSubReport).toList().size());
     System.out.println(reports.stream().filter(this::reportIsSafe).toList().size());
   }
 
   private boolean reportIsSafe(List<Long> report) {
+    boolean safe = checkSubReport(report);
+    if (safe) {
+      return true;
+    } else {
+      List<List<Long>> subreports = new ArrayList<>();
+      for (int i = 0; i < report.size(); i++) {
+        List<Long> newReport = new ArrayList<>(report);
+        newReport.remove(i);
+        subreports.add(newReport);
+      }
+      return subreports.stream().anyMatch(this::checkSubReport);
+    }
+  }
+
+  private boolean checkSubReport(List<Long> report) {
     boolean ascending = false;
     boolean descending = false;
     boolean invalidGap = false;
