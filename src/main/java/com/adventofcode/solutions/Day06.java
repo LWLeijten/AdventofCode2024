a7f869dba529ad6f40dcba6478852f36fe2d9c96
LWLeijten <larsleijten@gmail.com>
2024-12-08 22:49:29 +0100

    Day 6 part two

--- src/main/java/com/adventofcode/solutions/Day06.java
+++ src/main/java/com/adventofcode/solutions/Day06.java
@@ -5,38 +5,83 @@ import com.adventofcode.utils.InputReader;
 import com.adventofcode.utils.Tuple;
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Day06 {
   public Day06() throws FileNotFoundException, URISyntaxException {
     List<String> map = InputReader.readInput("/day06.txt");
-    HashMap<Tuple<Integer>, ArrayList<Direction>> visited = new HashMap<>();
-    Direction direction = Direction.NORTH;
-    Tuple<Integer> position = getStartPosition(map);
 
-    // part one
+    HashMap<Tuple<Integer>, ArrayList<Direction>> visited = partOne(map);
+    System.out.printf("Part one: %s%n", visited.size());
+    System.out.printf("Part two: %s%n", partTwo(visited, map));
+  }
+
+  private HashMap<Tuple<Integer>, ArrayList<Direction>> partOne(List<String> map) {
+    HashMap<Tuple<Integer>, ArrayList<Direction>> visited = new HashMap<>();
+    LocationDirection locationDirection =
+        new LocationDirection(getStartPosition(map), Direction.NORTH);
     while (true) {
       try {
-        if (!visited.containsKey(position)) {
-          visited.put(position, new ArrayList<>(List.of(direction)));
-        } else {
-          visited.get(position).add(direction);
-        }
-
-        Tuple<Integer> newPosition = getNewPosition(direction, position);
-        if (map.get(newPosition.elem2()).charAt(newPosition.elem1()) == '#') {
-          direction = direction.getNextDirection();
-        } else {
-          position = newPosition;
-        }
+        locationDirection = doStep(locationDirection, visited, map);
       } catch (IndexOutOfBoundsException e) {
         break;
       }
     }
-    System.out.printf("Part one: %s%n", visited.size());
+    return visited;
+  }
+
+  private int partTwo(
+      HashMap<Tuple<Integer>, ArrayList<Direction>> possibleObstacles, List<String> map) {
+    HashSet<Tuple<Integer>> loopingObstacles = new HashSet<>();
+    for (Tuple<Integer> obstacle : possibleObstacles.keySet()) {
+      ArrayList<String> newMap = placeObstacle(new ArrayList<>(map), obstacle);
+      LocationDirection locationDirection =
+          new LocationDirection(getStartPosition(map), Direction.NORTH);
+      HashMap<Tuple<Integer>, ArrayList<Direction>> visited = new HashMap<>();
+      while (true) {
+        try {
+          if (visited.containsKey(locationDirection.location())
+              && visited
+                  .get(locationDirection.location())
+                  .contains(locationDirection.direction())) {
+            loopingObstacles.add(obstacle);
+            break;
+          }
+          locationDirection = doStep(locationDirection, visited, newMap);
+        } catch (IndexOutOfBoundsException e) {
+          break;
+        }
+      }
+    }
+    return loopingObstacles.size();
+  }
+
+  private LocationDirection doStep(
+      LocationDirection locationDirection,
+      HashMap<Tuple<Integer>, ArrayList<Direction>> visited,
+      List<String> map) {
+    Tuple<Integer> position = locationDirection.location();
+    Direction direction = locationDirection.direction();
+    if (!visited.containsKey(position)) {
+      visited.put(position, new ArrayList<>(List.of(direction)));
+    } else {
+      visited.get(position).add(direction);
+    }
+    Tuple<Integer> newPosition = getNewPosition(direction, position);
+    if (map.get(newPosition.elem2()).charAt(newPosition.elem1()) == '#') {
+      direction = direction.getNextDirection();
+    } else {
+      position = newPosition;
+    }
+    return new LocationDirection(position, direction);
+  }
+
+  private ArrayList<String> placeObstacle(ArrayList<String> map, Tuple<Integer> obstacle) {
+    String row = map.get(obstacle.elem2());
+    map.set(
+        obstacle.elem2(),
+        row.substring(0, obstacle.elem1()) + '#' + row.substring(obstacle.elem1() + 1));
+    return map;
   }
 
   private Tuple<Integer> getNewPosition(Direction direction, Tuple<Integer> position) {
@@ -68,3 +113,5 @@ public class Day06 {
     throw new RuntimeException("No starting position found.");
   }
 }
+
+record LocationDirection(Tuple<Integer> location, Direction direction) {}
