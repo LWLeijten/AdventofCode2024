81293706146e1c9353eb7b45ed110690cdcd6e94
LWLeijten <larsleijten@gmail.com>
2024-12-08 15:05:48 +0100

    Refactor day 7

--- src/main/java/com/adventofcode/solutions/Day07.java
+++ src/main/java/com/adventofcode/solutions/Day07.java
@@ -2,38 +2,34 @@ package com.adventofcode.solutions;
 
 import com.adventofcode.utils.InputReader;
 import com.adventofcode.utils.Node;
-
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class Day07 {
-  // TODO: Clean up, pretty print
-  // Create operators +, *, || and pass these ass parameters to one generateTree function.
+  BiFunction<Long, Long, Long> add = Long::sum;
+  BiFunction<Long, Long, Long> multiply = (a, b) -> a * b;
+  BiFunction<Long, Long, Long> concat = (a, b) -> Long.parseLong(a.toString().concat(b.toString()));
+
   public Day07() throws FileNotFoundException, URISyntaxException {
     List<String> input = InputReader.readInput("/day07.txt");
-    Long result = 0L;
+    System.out.printf("Part one: %s%n", solve(input, List.of(add, multiply)));
+    System.out.printf("Part two: %s%n", solve(input, List.of(add, multiply, concat)));
+  }
+
+  private Long solve(List<String> input, List<BiFunction<Long, Long, Long>> operators) {
+    long result = 0L;
     for (String i : input) {
       long goal = Long.parseLong(i.split(":")[0]);
       List<Long> numbers =
           Arrays.stream((i.split(": ")[1]).split(" ")).map(Long::parseLong).toList();
-      Node<Long> tree = generateTree(0, numbers.get(0), numbers);
+      Node<Long> tree = generateTree(0, numbers.get(0), numbers, operators, goal);
       if (treeContainsLeafWithValue(tree, goal)) {
-        result+=goal;
+        result += goal;
       }
     }
-    System.out.println(result);
-    result = 0L;
-    for (String i : input) {
-      long goal = Long.parseLong(i.split(":")[0]);
-      List<Long> numbers =
-              Arrays.stream((i.split(": ")[1]).split(" ")).map(Long::parseLong).toList();
-      Node<Long> tree = generateTreeTwo(0, numbers.get(0), numbers);
-      if (treeContainsLeafWithValue(tree, goal)) {
-        result+=goal;
-      }
-    }
-    System.out.println(result);
+    return result;
   }
 
   public boolean treeContainsLeafWithValue(Node<Long> start, Long value) {
@@ -43,41 +39,36 @@ public class Day07 {
       Node<Long> curr = queue.remove();
       if (curr.getChildren().isEmpty() && curr.getValue().equals(value)) {
         return true;
-      } else if (curr.getValue() > value) {
-        continue;
       }
       queue.addAll(curr.getChildren());
     }
     return false;
   }
 
-  public Node<Long> generateTree(Integer depth, Long value, List<Long> numbers) {
+  public Node<Long> generateTree(
+      Integer depth,
+      Long value,
+      List<Long> numbers,
+      List<BiFunction<Long, Long, Long>> operators,
+      Long goal) {
     Node<Long> node;
-    if (depth == numbers.size() - 1) {
+    // Cancel recursion if we hit max-depth or value already higher than goal
+    if (depth == numbers.size() - 1 || value > goal) {
       node = new Node<>(value, List.of());
     } else {
       node =
           new Node<>(
               value,
-              List.of(
-                  generateTree(depth + 1, value + numbers.get(depth + 1), numbers),
-                  generateTree(depth + 1, value * numbers.get(depth + 1), numbers)));
-    }
-    return node;
-  }
-
-  public Node<Long> generateTreeTwo(Integer depth, Long value, List<Long> numbers) {
-    Node<Long> node;
-    if (depth == numbers.size() - 1) {
-      node = new Node<>(value, List.of());
-    } else {
-      node =
-              new Node<>(
-                      value,
-                      List.of(
-                              generateTreeTwo(depth + 1, value + numbers.get(depth + 1), numbers),
-                              generateTreeTwo(depth + 1, value * numbers.get(depth + 1), numbers),
-                              generateTreeTwo(depth + 1, Long.parseLong(value.toString().concat(numbers.get(depth + 1).toString())), numbers)));
+              operators.stream()
+                  .map(
+                      op ->
+                          generateTree(
+                              depth + 1,
+                              op.apply(value, numbers.get(depth + 1)),
+                              numbers,
+                              operators,
+                              goal))
+                  .toList());
     }
     return node;
   }
