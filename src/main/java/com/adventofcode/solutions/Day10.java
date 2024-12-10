61ef4bb71e3fbb32a4e0057d6755c74b68668a78
Lars Leijten <larsleijten@gmail.com>
2024-12-10 14:28:58 +0100

    Day 10

---
+++ src/main/java/com/adventofcode/solutions/Day10.java
@@ -0,0 +1,93 @@
+package com.adventofcode.solutions;
+
+import com.adventofcode.utils.InputReader;
+import com.adventofcode.utils.Tuple;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class Day10 {
+  public Day10() throws FileNotFoundException, URISyntaxException {
+    List<String> input = InputReader.readInput("/day10.txt");
+    List<List<Long>> map = inputToLongMap(input);
+    HashSet<Tuple<Integer>> startingPoints = getStartingPoints(map);
+    System.out.printf("Part one: %s%n", scoreTrailHeads(startingPoints, map, true));
+    System.out.printf("Part two: %s%n", scoreTrailHeads(startingPoints, map, false));
+  }
+
+  private long scoreTrailHeads(HashSet<Tuple<Integer>> startingPoints, List<List<Long>> map,
+                               boolean trackVisits) {
+    HashMap<Tuple<Integer>, Long> trails = new HashMap<>();
+    startingPoints.forEach(p -> trails.put(p, 0L));
+
+    for (Tuple<Integer> startingPoint : startingPoints) {
+      Queue<Tuple<Integer>> queue = new LinkedList<>(List.of(startingPoint));
+      Set<Tuple<Integer>> visited = new HashSet<>(List.of(startingPoint));
+
+      while (!queue.isEmpty()) {
+        Tuple<Integer> currentPoint = queue.remove();
+        Long currentHeight = map.get(currentPoint.elem2()).get(currentPoint.elem1());
+        if (currentHeight == 9) {
+          trails.put(startingPoint, trails.get(startingPoint) + 1);
+          continue;
+        }
+        List<Tuple<Integer>> neighbours = getNeighbours(currentPoint);
+        neighbours.stream()
+            .filter(nb -> (!visited.contains(nb) || !trackVisits)
+                && coordinateIsInBounds(nb, map)
+                && map.get(nb.elem2()).get(nb.elem1()) == currentHeight + 1)
+            .forEach(nb -> {
+              queue.offer(nb);
+              visited.add(nb);
+            });
+      }
+    }
+    return trails.values().stream().reduce(0L, Long::sum);
+  }
+
+  private boolean coordinateIsInBounds(Tuple<Integer> coord, List<List<Long>> map) {
+    return coord.elem1() >= 0
+        && coord.elem1() < map.getFirst().size()
+        && coord.elem2() >= 0
+        && coord.elem2() < map.size();
+  }
+
+  private List<Tuple<Integer>> getNeighbours(Tuple<Integer> currentPoint) {
+    return new ArrayList<>(List.of(
+        new Tuple<>(currentPoint.elem1(), currentPoint.elem2() + 1),
+        new Tuple<>(currentPoint.elem1(), currentPoint.elem2() - 1),
+        new Tuple<>(currentPoint.elem1() + 1, currentPoint.elem2()),
+        new Tuple<>(currentPoint.elem1() - 1, currentPoint.elem2())
+    ));
+  }
+
+  private HashSet<Tuple<Integer>> getStartingPoints(List<List<Long>> map) {
+    HashSet<Tuple<Integer>> startingPoints = new HashSet<>();
+    for (int y = 0; y < map.size(); y++) {
+      for (int x = 0; x < map.get(y).size(); x++) {
+        if (map.get(y).get(x) == 0) {
+          startingPoints.add(new Tuple<>(x, y));
+        }
+      }
+    }
+    return startingPoints;
+  }
+
+  private List<List<Long>> inputToLongMap(List<String> input) {
+    List<List<Long>> map = new ArrayList<>();
+    input.forEach(row -> {
+      List<Long> list = new ArrayList<>();
+      for (char c : row.toCharArray()) {
+        list.add((long) Character.getNumericValue(c));
+      }
+      map.add(list);
+    });
+    return map;
+  }
+}
